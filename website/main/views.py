cb3a0c487a34790bc4bf74654c033cc31ca9bb02
Emil Ridulfo <emilridulfo@gmail.com>
2020-09-20 15:17:26 +0200

    Initial commit

---
+++ website/main/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
+#from bokeh.io import curdoc
+
+import json
+import os
+import datetime
+# Create your views here.
+    
+
+def index(request):
+    content = json.load(open("../data.json"))
+    content["title"] = "Home"
+
+
+    plot = figure(title="Line graph", x_axis_label="Time", y_axis_label="Value", plot_width=800, plot_height=250,sizing_mode='stretch_width')
+    #curdoc().theme = 'dark_minimal'
+    #curdoc().add_root(plot)
+    plot.toolbar.logo = None
+    #plot.toolbar_location = None
+    plot.line(
+        [int(x) for x in content["nav"]["datetime"]],
+        [float(x) for x in content["nav"]["values"]],
+        line_width=2)
+    scripts, div = components(plot)
+    content["script"] = scripts
+    content["div"] = div
+
+    return render(request, 'home/index.html', content)
+
+def blog(request):
+    entries = []
+    for filename in os.listdir("../blog_entries/"):
+        with open("../blog_entries/" + filename, "r") as f:
+            date = datetime.datetime.strptime(filename.replace(".txt", ""), "%Y-%m-%d").date()
+            entries.append((date, f.read()))
+    entries.sort()
+    entries = entries[::-1]
+
+    content = {"blogs": []}
+    for entry in entries:
+        blog = {"date":entry[0]}
+        text = entry[1]
+        blog["title"] = text.split("\n")[0]
+        blog["body"] = "\n".join(text.split("\n")[2:])
+        content["blogs"].append(blog)
+
+    content["title"] = "Blog"
+    return render(request, "blog/blog.html", content)
+
+def code(request):
+    content = {}
+    content["title"] = "Code"
+    return render(request, "code/code.html", content)
+
+def about(request):
+    content = {}
+    content["title"] = "About"
+    return render(request, "about/about.html", content)
+
+def contact(request):
+    content = {}
+    content["title"] = "Contact"
+    return render(request, "contact/contact.html", content)
