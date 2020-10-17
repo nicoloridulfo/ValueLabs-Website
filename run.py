d3b4afb51a43ede685380a04f16b7b283ecc07bd
Emil Ridulfo <emilridulfo@gmail.com>
2020-10-17 22:05:43 +0200

    Reimplemented as flask

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from flask import Flask, render_template
 from bokeh.plotting import figure, output_file, show 
 from bokeh.embed import components
 #from bokeh.io import curdoc
@@ -7,13 +6,13 @@ from bokeh.embed import components
 import json
 import os
 import datetime
-# Create your views here.
-    
 
-def index(request):
-    content = json.load(open("../data.json"))
-    content["title"] = "Home"
 
+app = Flask(__name__, template_folder=".")
+
+@app.route("/")
+def home():
+    content = json.load(open("data.json"))
 
     plot = figure(title="Line graph", x_axis_label="Time", y_axis_label="Value", plot_width=800, plot_height=250,sizing_mode='stretch_width')
     #curdoc().theme = 'dark_minimal'
@@ -28,18 +27,15 @@ def index(request):
     content["script"] = scripts
     content["div"] = div
 
-    return render(request, 'home/index.html', content)
-
-def blog(request):
     entries = []
-    for filename in os.listdir("../blog_entries/"):
-        with open("../blog_entries/" + filename, "r") as f:
+    for filename in os.listdir("blog_entries/"):
+        with open("blog_entries/" + filename, "r") as f:
             date = datetime.datetime.strptime(filename.replace(".txt", ""), "%Y-%m-%d").date()
             entries.append((date, f.read()))
     entries.sort()
     entries = entries[::-1]
 
-    content = {"blogs": []}
+    content["blogs"] = []
     for entry in entries:
         blog = {"date":entry[0]}
         text = entry[1]
@@ -47,20 +43,7 @@ def blog(request):
         blog["body"] = "\n".join(text.split("\n")[2:])
         content["blogs"].append(blog)
 
-    content["title"] = "Blog"
-    return render(request, "blog/blog.html", content)
+    return render_template("base.html", **content)
 
-def code(request):
-    content = {}
-    content["title"] = "Code"
-    return render(request, "code/code.html", content)
-
-def about(request):
-    content = {}
-    content["title"] = "About"
-    return render(request, "about/about.html", content)
-
-def contact(request):
-    content = {}
-    content["title"] = "Contact"
-    return render(request, "contact/contact.html", content)
+if __name__ == "__main__":
+    app.run(debug=True)
