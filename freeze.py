2f95848d5ef51ca72a3ce9ae1e13b2bf30e9920a
emilrid <emilridulfo@gmail.com>
2020-10-18 08:43:58 +0200

    Rename build/ to docs/ after freeze

--- freeze.py
+++ freeze.py
@@ -1,7 +1,9 @@
 from flask_frozen import Freezer
 from run import app
+from os import rename
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
+    rename("build/", "docs/")
